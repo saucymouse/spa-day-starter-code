57494317ff395efcaca08db16218f2c600724ba3
Will Staton <will.staton@gmail.com>
2021-05-27 00:05:06 -0400

    got some of the way thru

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,38 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("title", "Add User");
+        return "user/add.html";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("password", user.getPassword());
+
+        if (user.getPassword().equals(verify)) {
+            return "user/index.html";
+        } else {
+            return "user/add.html";
+        }
+
+
+    }
+
+}
+
