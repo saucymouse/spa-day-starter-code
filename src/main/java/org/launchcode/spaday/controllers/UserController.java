6f27b39f9e3f19fdd6be15d678f57b83ab3492eb
Will Staton <will.staton@gmail.com>
2021-06-04 21:15:20 -0400

    messing around with passing around user object

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -9,17 +9,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("/add")
-    public String displayAddUserForm() {
+    @GetMapping("add")
+    public String displayAddUserForm(Model model) {
+        User user = new User();
+        model.addAttribute(user);
         return "user/add";
     }
 
-    @PostMapping
+    @PostMapping("add")
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+//        model.addAttribute("user", user);
+//        model.addAttribute("verify", verify);
+//        model.addAttribute("username", user.getUsername());
+//        model.addAttribute("email", user.getEmail());
+
         if (user.getPassword().equals(verify)) {
            return "user/index";
         }
@@ -29,6 +32,5 @@ public class UserController {
         }
 
     }
-
 
 }
